34ca67ef73738b1f493b95f73fc646f6d8de1ac8
BelyakovArkadiy <belyakovarkadiy@gmail.com>
2021-02-26 19:23:33 +0600

    modified:   build.gradle 	modified:   src/test/java/ru/netology/domain/CashbackHackerTest.java

--- src/test/java/ru/netology/domain/CashbackHackerTest.java
+++ src/test/java/ru/netology/domain/CashbackHackerTest.java
@@ -1,8 +1,8 @@
 package ru.netology.domain;
+import org.junit.Test;
 
-import org.testng.annotations.Test;
+import static org.junit.Assert.assertEquals;
 
-import static org.testng.Assert.*;
 
 public class CashbackHackerTest {
     public CashbackHacker cashbackHacker = new CashbackHacker();
@@ -21,13 +21,13 @@ public class CashbackHackerTest {
     }
     @Test
     void shouldCashBackSum5000() {
-        int actual = cashbackHacker.remain(5000);  // Тест падает
+        int actual = cashbackHacker.remain(5000);
         int expected = 0;
         assertEquals(actual,expected);
     }
     @Test
     void shouldCashBackSum1000() {
-        int actual = cashbackHacker.remain(1000); // Тест падает
+        int actual = cashbackHacker.remain(1000);// test failed
         int expected = 0;
         assertEquals(actual,expected);
     }
